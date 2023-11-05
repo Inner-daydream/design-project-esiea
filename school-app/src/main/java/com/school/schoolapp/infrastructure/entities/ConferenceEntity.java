ad3e55bc59dba6974d01ae53f5d48950f549684a
lucas <contact@lucasquitman.fr>
2023-11-05 17:10:14 +0100

    Working speaker_id

--- school-app/src/main/java/com/school/schoolapp/infrastructure/entities/ConferenceEntity.java
+++ school-app/src/main/java/com/school/schoolapp/infrastructure/entities/ConferenceEntity.java
@@ -1,10 +1,11 @@
 package com.school.schoolapp.infrastructure.entities;
 import com.school.schoolapp.domain.implementations.Conference;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
-
 @Entity
 public class ConferenceEntity {
     @Id
@@ -12,11 +13,8 @@ public class ConferenceEntity {
     @OneToOne
     @JoinColumn(name = "event_id")
     private EventEntity event;
-    private String speaker;
-
-    public String getSpeaker() {
-        return speaker;
-    }
+    @Column(name = "speaker_id")
+    private String speakerID;
 
     public ConferenceEntity() {
     }
@@ -24,6 +22,6 @@ public class ConferenceEntity {
     public ConferenceEntity(Conference conference) {
         this.ID = conference.getId().toString();
         this.event = new EventEntity(conference);
-        this.speaker = null;
+        this.speakerID = conference.getSpeaker().getId().toString();
     }
 }
