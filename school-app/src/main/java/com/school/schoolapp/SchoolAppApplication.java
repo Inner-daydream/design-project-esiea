f7d629c90d20b39f294455f067324c9c8797f784
lucas <contact@lucasquitman.fr>
2023-11-05 11:43:21 +0100

    removing unused imports

--- school-app/src/main/java/com/school/schoolapp/SchoolAppApplication.java
+++ school-app/src/main/java/com/school/schoolapp/SchoolAppApplication.java
@@ -1,14 +1,6 @@
 package com.school.schoolapp;
-
-import com.school.schoolapp.domain.implementations.School;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class SchoolAppApplication {
