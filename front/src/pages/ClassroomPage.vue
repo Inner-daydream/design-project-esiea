20ff42442fd9df4dbf09a997679495baeabb9a0e
JAM <jacquesarnomozet@gmail.com>
2023-11-06 09:51:54 +0100

    classroom front

---
+++ front/src/pages/ClassroomPage.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import ClassroomDisplay from 'src/components/ClassroomDisplay.vue';
+
+</script>
+<template>
+    <q-page class="row">
+        <ClassroomDisplay/>
+    </q-page>
+</template>
+
+
